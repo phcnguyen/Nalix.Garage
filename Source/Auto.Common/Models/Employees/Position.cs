460edf242a27aae52ccaabab14730024c8a7cc0e
PhcNguyen <phcnguyenz@proton.me>
2025-02-09 15:40:06 +0700

    0.0.10

--- Source/Auto.Common/Models/Employees/Position.cs
+++ Source/Auto.Common/Models/Employees/Position.cs
@@ -68,5 +68,125 @@ public enum Position
     /// <summary>
     /// Vị trí nhân viên bảo trì.
     /// </summary>
-    MaintenanceStaff = 12
+    MaintenanceStaff = 12,
+
+    /// <summary>
+    /// Vị trí điều phối kho.
+    /// </summary>
+    InventoryCoordinator = 13,
+
+    /// <summary>
+    /// Vị trí giám sát kho.
+    /// </summary>
+    WarehouseSupervisor = 14,
+
+    /// <summary>
+    /// Vị trí thợ sơn.
+    /// </summary>
+    Painter = 15,
+
+    /// <summary>
+    /// Vị trí chuyên viên chẩn đoán.
+    /// </summary>
+    DiagnosticSpecialist = 16,
+
+    /// <summary>
+    /// Vị trí chuyên viên động cơ.
+    /// </summary>
+    EngineSpecialist = 17,
+
+    /// <summary>
+    /// Vị trí chuyên viên hộp số.
+    /// </summary>
+    TransmissionSpecialist = 18,
+
+    /// <summary>
+    /// Vị trí chuyên viên điều hòa.
+    /// </summary>
+    ACSpecialist = 19,
+
+    /// <summary>
+    /// Vị trí thợ mài.
+    /// </summary>
+    Grinder = 20,
+
+    /// <summary>
+    /// Vị trí nhân viên bảo hiểm.
+    /// </summary>
+    InsuranceStaff = 21,
+
+    /// <summary>
+    /// Vị trí nhân viên bán phụ tùng.
+    /// </summary>
+    PartsConsultant = 22,
+
+    /// <summary>
+    /// Vị trí nhân viên giao nhận xe.
+    /// </summary>
+    VehicleDeliveryStaff = 23,
+
+    /// <summary>
+    /// Vị trí nhân viên vệ sinh.
+    /// </summary>
+    CleaningStaff = 24,
+
+    /// <summary>
+    /// Vị trí bảo vệ.
+    /// </summary>
+    Security = 25,
+
+    /// <summary>
+    /// Vị trí nhân viên marketing.
+    /// </summary>
+    MarketingStaff = 26,
+
+    /// <summary>
+    /// Vị trí nhân viên chăm sóc khách hàng.
+    /// </summary>
+    CustomerService = 27,
+
+    /// <summary>
+    /// Vị trí giám đốc kỹ thuật.
+    /// </summary>
+    TechnicalDirector = 28,
+
+    /// <summary>
+    /// Vị trí giám đốc dịch vụ.
+    /// </summary>
+    ServiceDirector = 29,
+
+    /// <summary>
+    /// Vị trí giám đốc điều hành.
+    /// </summary>
+    ExecutiveDirector = 30,
+
+    /// <summary>
+    /// Vị trí thợ điện tử và lập trình.
+    /// </summary>
+    ElectronicsAndProgrammingTechnician = 31,
+
+    /// <summary>
+    /// Vị trí chuyên viên kiểm tra chất lượng.
+    /// </summary>
+    QualityControlSpecialist = 32,
+
+    /// <summary>
+    /// Vị trí nhân viên đặt hàng phụ tùng.
+    /// </summary>
+    PartsOrderingStaff = 33,
+
+    /// <summary>
+    /// Vị trí chuyên viên bảo hành.
+    /// </summary>
+    WarrantySpecialist = 34,
+
+    /// <summary>
+    /// Vị trí nhân viên thu ngân.
+    /// </summary>
+    Cashier = 35,
+
+    /// <summary>
+    /// Vị trí trưởng ca.
+    /// </summary>
+    ShiftSupervisor = 36
 }
